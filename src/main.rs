d44f06001fc3992746637f69bec7b4c5a417f25a
CroxxN <saradgaihre19@gmail.com>
2023-08-25 22:39:56 +0545

    sets up echo struct
    This commit sets up the EchoRequest struct, to be used for sending
    echo request. An struct system may or may not work with [u8] buffers
    so have to see if it works or not.
    
    ~@CroxxN

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,35 @@ mod error;
 use cli_parse::get_args;
 use error::RingError;
 
+#[derive(Debug, PartialEq, Eq)]
+struct EchoRequest {
+    echo_type: u8,
+    code: u8,
+    checksum: [u8; 2],
+    identifier: [u8; 2],
+    seq_num: [u8; 2],
+    echo_data: [u8; 3],
+}
+
+impl EchoRequest {
+    fn new() -> Self {
+        Self {
+            echo_type: 8,
+            code: 0,
+            checksum: [0; 2],
+            identifier: [0; 2],
+            seq_num: [0; 2],
+            echo_data: [0; 3],
+        }
+    }
+    fn calc_checksum(&mut self) {
+        unimplemented!()
+    }
+    fn populate_data(&mut self) {
+        unimplemented!()
+    }
+}
+
 fn main() -> Result<(), RingError> {
     let arg: Vec<String> = env::args().collect();
     let mut url = get_args(arg)?;
@@ -20,7 +49,7 @@ fn main() -> Result<(), RingError> {
     };
     let socket = Socket::new(Domain::IPV4, Type::RAW, Some(Protocol::ICMPV4))?;
     socket.connect(&sock_addr.into())?;
-
+    let _echo = EchoRequest::new();
     Ok(())
 }
 // \x1b[1m
