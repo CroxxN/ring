ae9133a5239a76dcb14b5bc43a09a1c52672320e
CroxxN <saradgaihre19@gmail.com>
2023-09-01 23:28:29 +0545

    statistics mostly word + non-blocking
    This commit adds the statstics functionality of
    the Ring utility. It's now mostly possible to
    see the statistics of how many packets were sent,
    and how many were replied by the server.
    
    ~@CroxxN

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use socket2::{Domain, Protocol, Socket, Type};
 use std::{
     env,
     io::Read,
-    net::ToSocketAddrs,
+    net::{SocketAddr, ToSocketAddrs},
     sync::{atomic::AtomicBool, Arc},
     thread, time,
 };
@@ -15,8 +15,8 @@ use error::RingError;
 #[derive(Debug, PartialEq, Eq)]
 struct RingStats {
     packet_sent: i32,
-    successful: Arc<i32>,
-    loss: Arc<i32>,
+    successful: u32,
+    loss: u32,
     // Better than using `Duration` as `Instant` exits specially for this purpose
     time: time::Instant,
 }
@@ -25,8 +25,8 @@ impl Default for RingStats {
     fn default() -> Self {
         Self {
             packet_sent: 0,
-            successful: Arc::new(0),
-            loss: Arc::new(0),
+            successful: 0,
+            loss: 0,
             time: time::Instant::now(),
         }
     }
@@ -55,6 +55,40 @@ impl Default for EchoRequest {
             echo_data: b"MITTEN".to_owned(),
         }
     }
+}
+
+// Maybe in subsequent versions, the function below will be a generic one to get either IPV4 or IPV6
+// Could make it by adding a type parameter `<IPVersion>` in the function description, and make it generic over
+// IP versions
+
+// The function could look like:
+
+// fn get_ip_socket<IPVersion>(url: &str) -> Result<SocketAddr, RingError>{}
+// where `IPVersion` is
+// enum IPVersion {
+//     IPV4,
+//     IPV6
+// }
+// Better yet, the whole function could be separated over the another module entirely
+// So, this function is curretly unstable, and may break over time
+// TODO: Make this function generic over both IP versions
+
+fn ip4_socket(url: &str) -> Result<SocketAddr, RingError> {
+    let mut parsed_socket_vec = url.to_socket_addrs()?;
+    let sock_addr = match parsed_socket_vec.next() {
+        Some(addr) if addr.is_ipv4() => addr,
+        Some(_) => {
+            if let Some(addr) = parsed_socket_vec.last() {
+                addr
+            } else {
+                return Err(RingError::NetworkError);
+            }
+        }
+        None => {
+            return Err(RingError::NetworkError);
+        }
+    };
+    return Ok(sock_addr);
 }
 
 impl EchoRequest {
@@ -107,6 +141,12 @@ impl EchoRequest {
         //
         unimplemented!()
     }
+
+    //To get the compiler to stop shouting about `ByteParseError` not being used
+    fn _parse_error() -> RingError {
+        //TODO: Actaully implement this
+        RingError::ByteParseError
+    }
 }
 
 fn main() -> Result<(), RingError> {
@@ -115,26 +155,21 @@ fn main() -> Result<(), RingError> {
     if !url.contains(":") {
         url = format!("{}:0", url);
     }
-
-    let sock_addr = if let Some(dest) = url.to_socket_addrs()?.next() {
-        dest
-    } else {
-        println!("\x1b[1;31mFailed to parse url\x1b[0m");
-        return Err(RingError::NetworkError);
-    };
-
     // let parsed_url = if let Some(u) = url.split_once(":") {
     //     u.0
     // } else {
     //     &url
     // };
-
+    let sock_addr = ip4_socket(&url)?;
     // YES! ARC! Electric!
     let socket = Arc::new(Socket::new(
         Domain::IPV4,
         Type::RAW,
         Some(Protocol::ICMPV4),
     )?);
+    // socket.set_read_timeout(Some(time::Duration::new(1, 0)))?;
+    // Just set it to non-blocking
+    socket.set_nonblocking(true)?;
 
     match socket.connect(&sock_addr.into()) {
         Ok(()) => {}
@@ -142,36 +177,51 @@ fn main() -> Result<(), RingError> {
             println!("{e}");
         }
     };
+    println!(
+        // Terminal Color(VT100) Specification form (https://chrisyeh96.github.io/2020/03/28/terminal-colors.html)
+
+        "\n\x1b[1;32mRinging \x1b[0m\x1b[4;34m{}({})\x1b[0m \x1b[1;32mwith \x1b[1;37m{} bytes\x1b[0m\x1b[1;32m of data\x1b[0m",
+        url, sock_addr, 14
+
+    );
     let mut echo = EchoRequest::new();
     echo.calc_checksum();
     let mut buf = [0u8; 64];
-    let recv_socket = Arc::clone(&socket);
+
+    let mut recv_socket = socket.try_clone()?;
+
+    // This bit seems extremely hacky. I don't want to introduce new dependency for MPMC channel, as
+    // the std MPSC channel is not suitable for the task below.
+    //Also, use Condvar?
     let cont = Arc::new(AtomicBool::new(true));
     let cont_recv = Arc::clone(&cont);
-    let mut stats = RingStats::default();
-    // let arc_recieved = Arc::clone(&mut stats.successful);
-    thread::spawn(move || {
+    let cont_send = Arc::clone(&cont);
+
+    let handle = thread::spawn(move || {
+        let mut rtx = (0u32, 0u32);
         while cont_recv.load(std::sync::atomic::Ordering::Relaxed) {
-            match recv_socket.as_ref().read(&mut buf) {
+            match recv_socket.read(&mut buf) {
                 Ok(i) => {
                     // syncing of atomic bool lags a bit, so when Ctrl + C is pressed,
                     // a zero-sized buffer is returned. We check the length, and print
                     // only when it's greater than 20
                     if i > 20 {
                         println!("{} bytes successfully returned from the server", (i - 20));
+                        rtx.0 = rtx.0 + 1;
                     }
                 }
-                Err(e) => {
-                    println!("Encountered an Error: {e}")
-                }
+                Err(_) => {}
             }
         }
+        return rtx;
     });
+
     // Use a mut array of u8, so increasing the `seq_num` doesn't require creating a whole new copy of
     // bytes.
     let mut packet: [u8; 14] = [0; 14];
     echo.final_bytes(&mut packet);
-    let cont_send = Arc::clone(&cont);
+
+    // Weirdly, you have to clone the `cont` variabel here. If cloned inside the `FnMut`, the compiler shouts
 
     // The ctrlc crate takes a FnMut as an argument. We use Arc to store and load a boolen value to
     // determine when to stop running the loop
@@ -179,16 +229,10 @@ fn main() -> Result<(), RingError> {
     ctrlc::set_handler(move || cont_send.store(false, std::sync::atomic::Ordering::Relaxed))
         .expect("Failed to register callback");
 
-    println!(
-        // Terminal Color(VT100) Specification form (https://chrisyeh96.github.io/2020/03/28/terminal-colors.html)
-
-        "\n\x1b[1;32mRinging \x1b[0m\x1b[4;34m{}({})\x1b[0m \x1b[1;32mwith \x1b[1;37m{} bytes\x1b[0m\x1b[1;32m of data\x1b[0m",
-        url, sock_addr, 14
-    );
-
     // Starts measuring and taking stats
     // We initialize the stat struct here to be as correct as possible while measuring the time taken.
     // If we start early, the internal calculations may dilute the the time
+    let mut stats = RingStats::default();
 
     while cont.load(std::sync::atomic::Ordering::Relaxed) {
         match socket.send(&packet) {
@@ -200,11 +244,22 @@ fn main() -> Result<(), RingError> {
         }
     }
     // Add code here to give the diagnostics result of all the times Pinged.
-
+    let (sucsess, loss) = match handle.join() {
+        Ok((s, l)) => (s, l),
+        Err(_) => {
+            println!("It has panicked");
+            (0, 0)
+        }
+    };
+    stats.successful = sucsess;
+    stats.loss = loss;
     println!(
-        "\nSuccessfully Ringed {} packets! Now exiting!",
-        stats.packet_sent
+        "\nSuccessfully Ringed! Received {} packets of {} total packets, with {}% loss! Now exiting!",
+        stats.successful,
+        stats.packet_sent,
+        ((stats.loss * 100) / stats.packet_sent as u32)
     );
+
     // Free Up the socket just in case
     socket.shutdown(std::net::Shutdown::Both)?;
     Ok(())
