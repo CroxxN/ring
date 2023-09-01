fc361f066b6b4a5f61c91e71bf78f62918cc7ddf
CroxxN <saradgaihre19@gmail.com>
2023-09-01 12:37:56 +0545

    start work on statistics on exit + todo: make fast

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,32 @@ use std::{
     io::Read,
     net::ToSocketAddrs,
     sync::{atomic::AtomicBool, Arc},
-    thread,
+    thread, time,
 };
 mod cli_parse;
 mod error;
 use cli_parse::get_args;
 use error::RingError;
+
+#[derive(Debug, PartialEq, Eq)]
+struct RingStats {
+    packet_sent: i32,
+    successful: Arc<i32>,
+    loss: Arc<i32>,
+    // Better than using `Duration` as `Instant` exits specially for this purpose
+    time: time::Instant,
+}
+
+impl Default for RingStats {
+    fn default() -> Self {
+        Self {
+            packet_sent: 0,
+            successful: Arc::new(0),
+            loss: Arc::new(0),
+            time: time::Instant::now(),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Eq)]
 struct EchoRequest {
@@ -41,9 +61,11 @@ impl EchoRequest {
     fn new() -> Self {
         Self::default()
     }
+    #[inline]
     fn calc_checksum(&mut self) {
         let mut sum = 0u32;
-        let bytes = self.final_bytes();
+        let mut bytes = [0u8; 14];
+        self.final_bytes(&mut bytes);
         for word in bytes.chunks(2) {
             let mut part = u16::from(word[0]) << 8;
             if word.len() > 1 {
@@ -63,8 +85,7 @@ impl EchoRequest {
     fn _increase_seq(&mut self) {
         self.seq_num = self.seq_num + 1;
     }
-    fn final_bytes(&mut self) -> [u8; 14] {
-        let mut final_bytes: [u8; 14] = [0; 14];
+    fn final_bytes<'a>(&mut self, final_bytes: &mut [u8; 14]) {
         final_bytes[0] = self.echo_type;
         final_bytes[1] = self.code;
         final_bytes[2] = self.checksum[0];
@@ -79,7 +100,12 @@ impl EchoRequest {
         final_bytes[11] = self.echo_data[3];
         final_bytes[12] = self.echo_data[4];
         final_bytes[13] = self.echo_data[5];
-        final_bytes
+    }
+    // Will change `_ping_status()` to `ping_status()` currently uses
+    // underscore to get the compiler stop shouting
+    fn _ping_status() {
+        //
+        unimplemented!()
     }
 }
 
@@ -90,12 +116,18 @@ fn main() -> Result<(), RingError> {
         url = format!("{}:0", url);
     }
 
-    let sock_addr = if let Some(dest) = url.to_socket_addrs()?.last() {
+    let sock_addr = if let Some(dest) = url.to_socket_addrs()?.next() {
         dest
     } else {
         println!("\x1b[1;31mFailed to parse url\x1b[0m");
         return Err(RingError::NetworkError);
     };
+
+    // let parsed_url = if let Some(u) = url.split_once(":") {
+    //     u.0
+    // } else {
+    //     &url
+    // };
 
     // YES! ARC! Electric!
     let socket = Arc::new(Socket::new(
@@ -116,50 +148,63 @@ fn main() -> Result<(), RingError> {
     let recv_socket = Arc::clone(&socket);
     let cont = Arc::new(AtomicBool::new(true));
     let cont_recv = Arc::clone(&cont);
+    let mut stats = RingStats::default();
+    // let arc_recieved = Arc::clone(&mut stats.successful);
     thread::spawn(move || {
-        while cont_recv.load(std::sync::atomic::Ordering::SeqCst) {
+        while cont_recv.load(std::sync::atomic::Ordering::Relaxed) {
             match recv_socket.as_ref().read(&mut buf) {
                 Ok(i) => {
-                    println!(
-                        "\n{} bytes successfully returned from the server",
-                        i.wrapping_sub(20)
-                    )
+                    // syncing of atomic bool lags a bit, so when Ctrl + C is pressed,
+                    // a zero-sized buffer is returned. We check the length, and print
+                    // only when it's greater than 20
+                    if i > 20 {
+                        println!("{} bytes successfully returned from the server", (i - 20));
+                    }
                 }
                 Err(e) => {
                     println!("Encountered an Error: {e}")
                 }
             }
-            thread::sleep(std::time::Duration::new(1, 0));
         }
     });
-    let final_bytes = &echo.final_bytes();
-    // Currently Unrechable - Create a breaking condition
+    // Use a mut array of u8, so increasing the `seq_num` doesn't require creating a whole new copy of
+    // bytes.
+    let mut packet: [u8; 14] = [0; 14];
+    echo.final_bytes(&mut packet);
     let cont_send = Arc::clone(&cont);
 
     // The ctrlc crate takes a FnMut as an argument. We use Arc to store and load a boolen value to
     // determine when to stop running the loop
 
-    ctrlc::set_handler(move || cont_send.store(false, std::sync::atomic::Ordering::SeqCst))
+    ctrlc::set_handler(move || cont_send.store(false, std::sync::atomic::Ordering::Relaxed))
         .expect("Failed to register callback");
 
-    // Trial. Will see if the overhead of Arc exceeds that of channels, and if so, will replace with channels
-    while cont.load(std::sync::atomic::Ordering::SeqCst) {
-        match socket.send(final_bytes) {
-            Ok(i) => {
-                println!(
-                // Terminal Color(VT100) Specification form (https://chrisyeh96.github.io/2020/03/28/terminal-colors.html)
+    println!(
+        // Terminal Color(VT100) Specification form (https://chrisyeh96.github.io/2020/03/28/terminal-colors.html)
 
-                "\n\x1b[1;32mRinging \x1b[0m\x1b[4;34m{}({})\x1b[0m \x1b[1;32mwith \x1b[1;37m{} bytes\x1b[0m\x1b[1;32m of data\x1b[0m",
-                url, sock_addr, i
-                );
+        "\n\x1b[1;32mRinging \x1b[0m\x1b[4;34m{}({})\x1b[0m \x1b[1;32mwith \x1b[1;37m{} bytes\x1b[0m\x1b[1;32m of data\x1b[0m",
+        url, sock_addr, 14
+    );
+
+    // Starts measuring and taking stats
+    // We initialize the stat struct here to be as correct as possible while measuring the time taken.
+    // If we start early, the internal calculations may dilute the the time
+
+    while cont.load(std::sync::atomic::Ordering::Relaxed) {
+        match socket.send(&packet) {
+            Ok(_) => {
+                stats.packet_sent = stats.packet_sent + 1;
+                thread::sleep(std::time::Duration::new(1, 0));
             }
             Err(_) => return Err(RingError::NetworkError),
         }
-        thread::sleep(std::time::Duration::new(1, 0));
     }
     // Add code here to give the diagnostics result of all the times Pinged.
 
-    println!("\nSuccessfully Ringed! Now exiting!");
+    println!(
+        "\nSuccessfully Ringed {} packets! Now exiting!",
+        stats.packet_sent
+    );
     // Free Up the socket just in case
     socket.shutdown(std::net::Shutdown::Both)?;
     Ok(())
